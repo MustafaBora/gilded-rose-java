b11aa1a850d53ccb8a3f79b1ff704d02f57e477e
msimsek <mustafa-bora.simsek@capgemini.com>
2025-02-05 17:57:16 +0100

    First tests

---
+++ src/test/java/com/capgemini/GildedRoseTest.java
@@ -0,0 +1,15 @@
+package com.capgemini;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GildedRoseTest {
+
+
+    @Test
+    public void testUpdate() {
+        assertEquals(1,1);
+    }
+
+}
