b0d68b4433f50c5362f7d5d7a3d61243df638033
msimsek <mustafa-bora.simsek@capgemini.com>
2025-02-06 14:33:06 +0100

    isSulfuras check

--- src/main/java/com/capgemini/GildedRose.java
+++ src/main/java/com/capgemini/GildedRose.java
@@ -13,53 +13,53 @@ class GildedRose {
             isAgedBire = item.name.equals("Aged Brie");
             isBackstage = item.name.equals("Backstage passes to a TAFKAL80ETC concert");
             isSulfuras = item.name.equals("Sulfuras, Hand of Ragnaros");
-            if (!isAgedBire && !isBackstage) {
-                if (item.quality > 0) {
-                    if (!isSulfuras) {
+            if(isSulfuras) {
+                //don't do anything
+            }
+            else {
+                if (!isAgedBire && !isBackstage) {
+                    if (item.quality > 0) {
                         item.quality = item.quality - 1;
-                    }
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                    if (isBackstage) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!isSulfuras) {
-                item.sellIn = item.sellIn - 1;
-            }
-
-            if (item.sellIn < 0) {
-                if (!isAgedBire) {
-                    if (!isBackstage) {
-                        if (item.quality > 0) {
-                            if (!isSulfuras) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        item.quality = 0;
                     }
                 } else {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1;
+
+                        if (isBackstage) {
+                            if (item.sellIn < 11) {
+                                if (item.quality < 50) {
+                                    item.quality = item.quality + 1;
+                                }
+                            }
+
+                            if (item.sellIn < 6) {
+                                if (item.quality < 50) {
+                                    item.quality = item.quality + 1;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                item.sellIn = item.sellIn - 1;
+
+                if (item.sellIn < 0) {
+                    if (!isAgedBire) {
+                        if (!isBackstage) {
+                            if (item.quality > 0) {
+                                item.quality = item.quality - 1;
+                            }
+                        } else {
+                            item.quality = 0;
+                        }
+                    } else {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
                     }
                 }
             }
+
         }
     }
 }
