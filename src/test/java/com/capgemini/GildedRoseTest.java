2bb93d9ddc82b5a1a62114681ac07bc5509eea9d
msimsek <mustafa-bora.simsek@capgemini.com>
2025-02-06 17:56:55 +0100

    Added regression tests for backstage passes.

--- src/test/java/com/capgemini/GildedRoseTest.java
+++ src/test/java/com/capgemini/GildedRoseTest.java
@@ -16,7 +16,10 @@ public class GildedRoseTest {
                 new Item("Sulfuras, Hand of Ragnaros", 0, 45),
                 new Item("Aged Brie", 10, 0),
                 new Item("Aged Brie", 10, 50),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 9, 15),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 11, 15),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 7, 15),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 3, 15),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 0, 15),
                 new Item("Iron Man", 7, 25),
                 new Item("Black Widow", 0, 20),
                 new Item("Hawkeye", 0, 0)
@@ -28,22 +31,22 @@ public class GildedRoseTest {
     @Test
     public void testQualityAndSellInDecreasesForNormals() {
         app.updateQuality();
-        assertEquals(24, items[4].quality);
-        assertEquals(6, items[4].sellIn);
+        assertEquals(24, items[7].quality);
+        assertEquals(6, items[7].sellIn);
     }
 
     //Once the sell by date has passed, Quality degrades twice as fast
     @Test
     public void testQualityDecreasesTwiceAsFastAfterSellInForNormals() {
         app.updateQuality();
-        assertEquals(18, items[5].quality);
+        assertEquals(18, items[8].quality);
     }
 
     //The Quality of an item is never negative
     @Test
     public void testQualityIsNeverNegativeForNormals() {
         app.updateQuality();
-        assertEquals(0, items[6].quality);
+        assertEquals(0, items[9].quality);
     }
 
     // “Aged Brie” actually increases in Quality the older it gets
@@ -65,6 +68,34 @@ public class GildedRoseTest {
     public void testSulfurasNeverDecreasesInQuality() {
         app.updateQuality();
         assertEquals(45, items[0].quality);
+    }
+
+    //“Backstage passes”, like aged brie, increases in Quality as it’s SellIn value approaches;
+    @Test
+    public void testBackstageIncreasesQualityByOneWhenEarly() {
+        app.updateQuality();
+        assertEquals(16, items[3].quality);
+    }
+
+    //“Backstage passes” -> Quality increases by 2 when there are 10 days or less
+    @Test
+    public void testBackstageIncreasesQualityByOneWhen10DaysOrLess() {
+        app.updateQuality();
+        assertEquals(17, items[4].quality);
+    }
+
+    //“Backstage passes” -> Quality increases by 2 when there are 10 days or less
+    @Test
+    public void testBackstageIncreasesQualityByOneWhen5DaysOrLess() {
+        app.updateQuality();
+        assertEquals(18, items[5].quality);
+    }
+
+    //“Backstage passes” -> but Quality drops to 0 after the concert
+    @Test
+    public void testBackstageQualityDropsToZeroAfterConcert() {
+        app.updateQuality();
+        assertEquals(0, items[6].quality);
     }
 
 }
